c8efede587df42b3e55fb1f6d8ff69c1d8a542c3
sohjiro <felipe@makingdevs.com>
2014-11-27 17:03:54 -0600

    método de visualización de una factura dependendiendo si se quiere el xml o pdf

--- src/main/groovy/com/tim/one/billing/controller/FacturacionController.groovy
+++ src/main/groovy/com/tim/one/billing/controller/FacturacionController.groovy
@@ -1,15 +1,36 @@
 package com.tim.one.billing.controller
 
 import org.springframework.web.bind.annotation.*
+import org.springframework.beans.factory.annotation.Autowired
+import com.tim.one.billing.services.FacturaServicio
+
+import javax.servlet.http.HttpServletResponse
+import org.springframework.util.FileCopyUtils
 
 @RestController
 @RequestMapping("/facturacion")
 class FacturacionController {
 
+  @Autowired
+  FacturaServicio facturaServicio
 
   @RequestMapping(method = RequestMethod.GET)
-  @ResponseBody def show() {
-    "Showing factura"
+  def show(FacturaShowCommand command, HttpServletResponse response) {
+    String format = command.format.toLowerCase().capitalize()
+    def factura = facturaServicio."show${format}FacturaWithFolio"(command.folio, command.format)
+    def file = new File('file.xml')
+    def fis = new FileInputStream(file)
+
+    response.setContentType("application/xml")
+    response.setContentLength(((int) file.size()))
+    response.setHeader("Content-Disposition","attachment filename=\"" + file.getName() +"\"")
+    FileCopyUtils.copy(fis, response.getOutputStream())
+
+    return null
   }
 }
 
+class FacturaShowCommand {
+  String folio
+  String format
+}
