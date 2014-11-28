8c180b9aa3276d3714d467727f7b630414647c60
sohjiro <felipe@makingdevs.com>
2014-11-28 16:38:11 -0600

    creando el command para aceptar la petición de creación de factura

--- src/main/groovy/com/tim/one/billing/controller/FacturacionController.groovy
+++ src/main/groovy/com/tim/one/billing/controller/FacturacionController.groovy
@@ -3,6 +3,7 @@ package com.tim.one.billing.controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.beans.factory.annotation.Autowired
 import com.tim.one.billing.services.FacturaServicio
+import com.tim.one.billing.model.*
 
 import javax.servlet.http.HttpServletResponse
 import org.springframework.util.FileCopyUtils
@@ -20,16 +21,24 @@ class FacturacionController {
   }
 
   @RequestMapping(method = RequestMethod.POST, value="/create")
-  def createFacturaWithoutGeneratingFolio() {
-    println "WITHOUT FOLIO"
+  def createFacturaWithoutGeneratingFolio(FacturaCreateCommand command) {
+    def file = facturaServicio."genera${command.format}DeFactura"(command.datosDeFacturacion, command.contribuyenteEmisor, command.contribuyenteReceptor, command.conceptosAFacturar)
+
+    def fis = new FileInputStream(file)
+    response.setContentType("application/${command.format}")
+    response.setContentLength(((int) file.size()))
+    response.setHeader("Content-Disposition","attachment filename=\"" + file.name +"\"")
+    FileCopyUtils.copy(fis, response.getOutputStream())
+
+    null
   }
 
   @RequestMapping(method = RequestMethod.GET)
   def show(FacturaShowCommand command, HttpServletResponse response) {
     String format = command.format.toLowerCase().capitalize()
     def file = facturaServicio."show${format}FacturaWithFolio"(command.folio, command.format)
-    def fis = new FileInputStream(file)
 
+    def fis = new FileInputStream(file)
     response.setContentType("application/${command.format}")
     response.setContentLength(((int) file.size()))
     response.setHeader("Content-Disposition","attachment filename=\"" + file.getName() +"\"")
@@ -38,6 +47,16 @@ class FacturacionController {
     null
   }
 
+}
+
+class FacturaCreateCommand {
+  DatosDeFacturacion datosDeFacturacion
+  Contribuyente emisor
+  Contribuyente receptor
+
+  List<Concepto> conceptos
+  List<Impuesto> impuestos
+  String format = 'pdf'
 }
 
 class FacturaShowCommand {
