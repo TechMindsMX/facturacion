8a476d4233344ba42ece22b9173f32d5f6558a63
josdem <joseluis.delacruz@gmail.com>
2014-12-26 09:13:04 -0600

    #97 Agregando servicio de cancelacion de factura

---
+++ src/main/groovy/com/tim/one/billing/services/CancelaServicio.groovy
@@ -0,0 +1,7 @@
+package com.tim.one.billing.services
+
+interface CancelaServicio {
+	
+	void cancelaFactura(String uuid)
+	
+}
---
+++ src/main/groovy/com/tim/one/billing/services/impl/CancelaServicioImpl.groovy
@@ -0,0 +1,36 @@
+package com.tim.one.billing.services.impl
+
+import javax.annotation.PostConstruct
+
+import org.apache.commons.io.FileUtils
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+import views.core.soap.services.apps.Incidencia
+import views.core.soap.services.apps.IncidenciaArray
+
+import com.finkok.facturacion.stamp.StampSOAP
+import com.tim.one.billing.services.CadenaOriginalServicio
+import com.tim.one.billing.services.CancelaServicio
+import com.tim.one.billing.services.CfdiServicio;
+import com.tim.one.billing.services.SelloServicio
+import com.tim.one.billing.services.TimbraServicio
+import com.tim.one.billing.state.ApplicationState
+
+
+/**
+ * @author josdem
+ * @understands Service who know how to cancelar factura
+ *
+ */
+
+@Service
+class CancelaServicioImpl implements CancelaServicio {
+
+	@Override
+	public void cancelaFactura(String uuid) {
+	}
+	
+}
