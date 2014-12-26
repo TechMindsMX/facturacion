9ddcc527613cb3188540a1288b3490d8ce9f11c1
josdem <joseluis.delacruz@gmail.com>
2014-12-26 10:08:24 -0600

    #97 Rutina de cancelar factura agregada

--- src/main/groovy/com/tim/one/billing/collaborator/CancelaCollaborator.groovy
+++ src/main/groovy/com/tim/one/billing/collaborator/CancelaCollaborator.groovy
@@ -1,24 +1,18 @@
 package com.tim.one.billing.collaborator
 
-import javax.annotation.PostConstruct
+import javax.xml.bind.JAXBElement
 
-import org.apache.commons.io.FileUtils
-import org.apache.commons.logging.Log
-import org.apache.commons.logging.LogFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
-import views.core.soap.services.apps.Incidencia
-import views.core.soap.services.apps.IncidenciaArray
+import views.core.soap.services.apps.CancelaCFDResult
+import views.core.soap.services.apps.Folio
+import views.core.soap.services.apps.FolioArray
+import views.core.soap.services.apps.UUIDS
 
-import com.finkok.facturacion.stamp.StampSOAP
-import com.tim.one.billing.services.CadenaOriginalServicio
-import com.tim.one.billing.services.CfdiServicio;
-import com.tim.one.billing.services.SelloServicio
-import com.tim.one.billing.services.TimbraServicio
-import com.tim.one.billing.state.ApplicationState
-
+import com.finkok.facturacion.cancel.Application
+import com.finkok.facturacion.cancel.CancelSOAP
+import com.finkok.facturacion.cancel.ObjectFactory
+import com.finkok.facturacion.cancellation.StringArray
 
 /**
  * @author josdem
@@ -28,7 +22,51 @@ import com.tim.one.billing.state.ApplicationState
 
 @Component
 class CancelaCollaborator {
+	CancelSOAP cancelSOAP = new CancelSOAP()
+	Application application = cancelSOAP.getApplication()
+
 	void cancela(String uuid){
-		
+		ObjectFactory ob = new ObjectFactory();
+
+		UUIDS uuids = ob.createUUIDS();
+		StringArray sA = ob.createStringArray();
+		sA.getString().add("UUID");
+
+		JAXBElement<StringArray> array = ob.createUUIDSUuids(sA);
+		uuids.setUuids(array);
+
+		Openssl.creaCerPem();
+		Openssl.creaKeyPem();
+		Openssl.creaKeyEncriptado();
+
+		byte[] cer = null;
+		byte[] key = null;
+		try {
+			cer = Openssl.leeArchivo(Openssl.getRutaDestino() + "cer.pem").getBytes("UTF-8");
+			key = Openssl.leeArchivo(Openssl.getRutaDestino() + "key.enc").getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+		}
+
+		Openssl.deleteFiles();
+
+		CancelaCFDResult acuse = application.cancel(uuids, "usernameFinkok", "passwordFinkok", "RFCContribuyente", cer, key, true);
+
+		if (acuse.getAcuse() != null) {
+			System.out.println(acuse.getAcuse().getValue());
+		}
+		if (acuse.getCodEstatus() != null) {
+			System.out.println("Codigo Estatus: " + acuse.getCodEstatus().getValue());
+		}
+		if (acuse.getFolios() != null) {
+			FolioArray folioArray = acuse.getFolios().getValue();
+			for (Folio f : folioArray.getFolio()) {
+				if (f.getUUID() != null) {
+					System.out.println("UUID: " + f.getUUID().getValue());
+				}
+				if (f.getEstatusUUID() != null) {
+					System.out.println("Estatus UUID: " + f.getEstatusUUID().getValue());
+				}
+			}
+		}
 	}
 }
