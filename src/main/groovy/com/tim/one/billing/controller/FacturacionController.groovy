9f88962836aed381772b781d0e20016b4d4b28ba
josdem <joseluis.delacruz@gmail.com>
2015-03-25 10:15:19 -0600

    Changing to POST object

--- src/main/groovy/com/tim/one/billing/controller/FacturacionController.groovy
+++ src/main/groovy/com/tim/one/billing/controller/FacturacionController.groovy
@@ -49,8 +49,8 @@ class FacturacionController {
 	Log log = LogFactory.getLog(getClass())
 	
 	@RequestMapping(method = RequestMethod.POST, value="/create")
-	def createFacturaWithoutGeneratingFolio(@RequestBody String json, HttpServletResponse response) {
-		FacturaCreateCommand command = new Gson().fromJson(json, FacturaCreateCommand.class)
+	def createFacturaWithoutGeneratingFolio(FacturaCreateCommand command, HttpServletResponse response) {
+//		FacturaCreateCommand command = new Gson().fromJson(json, FacturaCreateCommand.class)
 		log.info("GENERATING factura")
 		log.info("command: " + command.dump())
 		def file = facturaServicio.generaXmlDeFactura(command.datosDeFacturacion, command.emisor, command.receptor, command.conceptos, command.impuestos)
