7284e2984b5db0391e650d9077fc298edb062d45
josdem <joseluis.delacruz@gmail.com>
2015-09-10 21:30:12 -0500

    Agregando las dependencias del jms

---
+++ src/main/groovy/com/tim/one/billing/messengine/LoggerMessageListener.groovy
@@ -0,0 +1,25 @@
+package com.tim.one.billing.messengine
+
+import javax.jms.JMSException
+import javax.jms.Message
+import javax.jms.MessageListener
+import javax.jms.ObjectMessage
+
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class LoggerMessageListener {
+
+	Log log = LogFactory.getLog(getClass())
+
+	def void onMessage(Message message) {
+		def data =  ((ObjectMessage) message).getObject()
+		String dataToString = data.collect { k, v -> v }.join(';')
+		log.info dataToString
+	}
+	
+}
